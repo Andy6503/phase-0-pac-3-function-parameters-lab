d78556a407ec7c523e3f11185e09073ae264555f
Andrew <andrew.kapsa1@gmail.com>
2021-10-03 15:32:04 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function introduction(string){
+    if (string === "Josh")
+    return ("Hi, my name is Josh.")
+    if (string === "Nancy")
+    return ("Hi, my name is Nancy.")
+}
+function introductionWithLanguage(name,language){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+function introductionWithLanguageOptional(name,language = "JavaScript"){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
